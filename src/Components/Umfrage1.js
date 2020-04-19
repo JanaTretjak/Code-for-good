ee76c7126bc45e5bfbcd375b5988aa6bf9004122
JanaTretjak <tretiachechka@gmail.com>
2020-04-19 13:59:48 +0200

    ver test7

--- src/Components/Umfrage1.js
+++ src/Components/Umfrage1.js
@@ -110,7 +110,7 @@ class Umfrage extends Component {
                                 <div id="array" name="array">
                                     <label name={`frage${i}`} value={elt.frage}>{elt.frage}</label>
                                     <label className="antwort" name={`antwort${i}`} value={elt.antwort}>{elt.antwort}</label>
-                                    {/* <input className="antwort" type="text" name={`antwort${i}`} value={elt.antwort} onChange={this.handleChange1} /> */}
+                                    <input className="antwort" type="text" name={`antwort${i}`} value={elt.antwort} onChange={this.handleChange1} />
                                 </div>)
                             }
                         </div>
