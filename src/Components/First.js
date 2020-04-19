89652a45c57011fde9960c94ba024d95ce8b6856
alexhonigmann <alexhonigmann@gmail.com>
2020-04-19 11:47:28 +0200

    ver 01

--- src/Components/First.js
+++ src/Components/First.js
@@ -67,7 +67,7 @@ class First extends Component {
                 <h1>Unsere Partner</h1>
                 <a href="#"><img src="../Components/img/blanko_logo.svg" alt=""></img></a>
                 <a href="#"><img src="./Components/img/super(code).png" alt=""></img></a>
-                <a href="#"><img src="//Components/img/blanko_logo.svg" alt=""></img></a>
+                <a href="#"><img src="./Components/img/blanko_logo.svg" alt=""></img></a>
                 <a href="#"><img src="./Components/img/image 10.png" alt=""></img></a>
                 <a href="#"><img src="assets/img/image 9.png" alt=""></img></a>
                 <a href="#"><img src="assets/img/sourcetree logo_png 1.png" alt=""></img></a>
