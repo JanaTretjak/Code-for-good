c883e6193ce27ecd7b63268a5d5608037b9a5759
JanaTretjak <tretiachechka@gmail.com>
2020-04-20 15:17:44 +0200

    ver test42

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -8,7 +8,8 @@ class Form extends Component {
             <form>
                 <div className={this.props.action ? "moveUp" : "up"}>
                     <label name={this.props.name}>{this.props.frage}</label>
-                    <input type="text" onChange={this.props.handleChange} name={`antwort${this.props.i}`} value={this.props.value} placeholder="schreibe hier deine Antwort..."></input>
+                    <input type="text" onChange={this.props.handleChange} name={`Name`} value={this.props.value} placeholder="schreibe hier deine Antwort..."></input>
+                    <input type="text" onChange={this.props.handleChange} name={`Vorname`} value={this.props.value} placeholder="schreibe hier deine Antwort..."></input>
                 </div>
                 <div className="bottom">
                     <button onClick={this.props.before}> <i class="fas fa-angle-down"></i> </button>
